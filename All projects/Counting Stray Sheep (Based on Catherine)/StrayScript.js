63580eae9ba67f343e58cded37ea57af2494658e
ChompskiHonk <77406368+ChompskiHonk@users.noreply.github.com>
2022-08-16 13:26:14 -0400

    Update StrayScript.js
    Updated script before clock out at work (there will be an email with code form codepen in case something goes wrong)

--- All projects/Counting Stray Sheep (Based on Catherine)/StrayScript.js
+++ All projects/Counting Stray Sheep (Based on Catherine)/StrayScript.js
@@ -2,11 +2,22 @@ window.addEventListener("load", () => {
   time();
   function time(){
     var curTime = new Date();
+    var hour = "";
     var sec = "";
+    var p = "";
     if (curTime.getSeconds() < 10){
       sec = "0"+curTime.getSeconds();
     } else sec += ""+curTime.getSeconds();
-    document.getElementById("SpookyTime").innerHTML = (("0"+curTime.getHours()+1).slice(-2)) + " : " + curTime.getMinutes() + " : "+ sec + " : "+ curTime.getMilliseconds();
+    
+    if ((("0"+curTime.getHours()+1).slice(-2)) >= 12){
+      p = "PM";
+    } else p = "AM";
+    
+    if(curTime.getHours() > 12) {
+      let h = curTime.getHours() - 12;
+      hour = ""+h;
+    }else hour = ""+curTime.getHours();
+    document.getElementById("SpookyTime").innerHTML = hour + " : " + curTime.getMinutes() + " : "+ sec + " : "+ curTime.getMilliseconds() + " "+ p;
     setTimeout(time, 10);
   }
 });
