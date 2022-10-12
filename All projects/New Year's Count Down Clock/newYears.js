029c154f64608256e86b956c630fb3c9eb2ae855
ChompskiHonk <77406368+ChompskiHonk@users.noreply.github.com>
2022-10-12 17:43:26 -0400

    Create newYears.js
    Added new javaScript file to accurately update the date and time on the webpage

---
+++ All projects/New Year's Count Down Clock/newYears.js
@@ -0,0 +1,28 @@
+window.addEventListener("load", function time(){
+  var curTime = new Date(), p  = "";
+  
+    var hour = "";
+    var min = "";
+    var sec = "";
+    var p = "";
+    if (curTime.getSeconds() < 10){
+      sec = "0"+curTime.getSeconds();
+    } else sec += ""+curTime.getSeconds();
+    
+    if (curTime.getMinutes() < 10){
+      min = "0"+curTime.getMinutes();
+    } else min += ""+curTime.getMinutes();
+    
+    if ((("0"+curTime.getHours()+1).slice(-2)) >= 12){
+      p = "PM";
+    } else p = "AM";
+    
+    if(curTime.getHours() > 12) {
+      let h = curTime.getHours() - 12;
+      hour = ""+h;
+    }else hour = ""+curTime.getHours();
+   
+  
+  document.getElementById('current-time').innerHTML = hour+":"+min+" "+p;
+    setTimeout(time, 10);
+});
