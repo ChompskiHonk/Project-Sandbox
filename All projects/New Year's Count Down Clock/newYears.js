216fbef08046bc2e7a0785e298f0ec869bb3d29e
ChompskiHonk <77406368+ChompskiHonk@users.noreply.github.com>
2022-10-17 18:40:17 -0400

    Update newYears.js
    Changed the javaScript so that the total hours minutes and seconds are shown without the days

--- All projects/New Year's Count Down Clock/newYears.js
+++ All projects/New Year's Count Down Clock/newYears.js
@@ -1,5 +1,5 @@
 function showRemaining(D) {
-        var end = new Date('01/01/2023 01:00 AM');
+        var end = new Date('01/01/2023 00:00 AM');
         var distance = end - D;
         if (distance < 0) {
 
@@ -11,13 +11,12 @@ function showRemaining(D) {
   var _second = 1000;
     var _minute = _second * 60;
     var _hour = _minute * 60;
-    var _day = _hour * 24;
-        var days = Math.floor(distance / _day);
-        var hours = Math.floor((distance % _day) / _hour);
+    
+        var hours = Math.floor(distance  / _hour);
         var minutes = Math.floor((distance % _hour) / _minute);
         var seconds = Math.floor((distance % _minute) / _second);
 
-        return days + 'days '+ hours + 'hrs '+ minutes + 'mins '+ seconds + 'secs';
+        return hours + 'hrs '+ minutes + 'mins '+ seconds + 'secs';
     }
 //code above provided by
 
